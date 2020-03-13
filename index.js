7ffa8f1c32c630ce847c2d5af301d13c7ed578de
Hanna Lee <mani21na@gmail.com>
2020-03-13 23:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const returnFirstTwoDrivers = drivers => drivers.slice(0,2);
 
 const returnLastTwoDrivers = drivers => drivers.slice(-2);
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
